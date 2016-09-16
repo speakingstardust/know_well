f6e22a0605f5d20a640d9147c697dcda72e74f2d
speakingstardust <josh@autochemky.com>
2016-09-16 19:39:51 -0400

    found validation issue causing problems with heroku, I think.

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,7 +3,7 @@ class Report < ActiveRecord::Base
 	has_many :jig_orders
 	belongs_to :customer
 
-	validates :start_date, :presence
-	validates :end_date, :presences 
+	validates :start_date, presence: true
+	validates :end_date, presence: true
 
 end
