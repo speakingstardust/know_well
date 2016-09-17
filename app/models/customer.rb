53d0107d3ae1be0cb43bb450b2050b0fd342d1e0
speakingstardust <josh@autochemky.com>
2016-09-16 21:17:43 -0400

    more removing reports

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,6 @@
 class Customer < ActiveRecord::Base
 	has_many :jigs
 	has_many :jig_orders
-	has_many :reports
 
 	validates :name, presence: true, uniqueness: true
 
--- app/models/jig_order.rb
+++ app/models/jig_order.rb
@@ -2,7 +2,6 @@ class JigOrder < ActiveRecord::Base
 	has_many :jig_order_line_items, :dependent => :destroy
 	has_many :jigs, through: :jig_order_line_items
 	belongs_to :customer
-	belongs_to :report
 
 	accepts_nested_attributes_for :jig_order_line_items, :reject_if => lambda { |a| a[:jig_id].blank? }, :allow_destroy => true
 
