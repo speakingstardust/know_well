543abf1b499da65d4c6d4df04debb7e36105493d
speakingstardust <josh@autochemky.com>
2017-01-12 14:58:41 -0500

    going to try to push to heroku to see if i still get access denied there.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,12 +51,14 @@ Rails.application.configure do
   # Settings for Paperclip - AWS Configuration
   config.paperclip_defaults = {
     storage: :s3,
+    s3_host_name: "#{ENV.fetch('S3_BUCKET_NAME')}.s3.amazonaws.com",
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION')
-    }
+    },
+    path: '/:class/:attachment/:id_partition/:style/:filename'
   }
 
   # Prepend all log lines with the following tags.
