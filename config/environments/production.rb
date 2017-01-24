56af7bb09dea97c0183d51bee17f78f9769b4b69
speakingstardust <josh@autochemky.com>
2017-01-24 11:22:55 -0500

    removed registerable and also got send grid setup in production environment for password resets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,9 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_chaching = false
+  confg.action_mailer.default_url_options = { host: "knowwell.herokuapp.com" }
+  config.action_mailer.delivery_method = :smtp
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
