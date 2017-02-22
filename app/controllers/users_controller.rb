b39d92b4f781448b758cbeac03170b692e13cf9c
speakingstardust <josh@autochemky.com>
2017-02-22 11:38:14 -0500

    Setup new, create, update, edit, and show actions for Admins to control Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,19 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :show, :destroy, :update]
-  before_action :authenticate_user!
+  before_action :authenticate_admin!
 
   def new
-
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
 
+    if @user.save
+      redirect_to users_path, notice: "User account successfully created for #{@user.full_name}."
+    else
+      render :new
+    end
   end
 
   def edit 
@@ -15,7 +21,15 @@ class UsersController < ApplicationController
   end
 
   def update 
-
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+    if @user.update(user_params)
+      redirect_to users_path, notice: "User account for #{@user.full_name} successfully updated."
+    else
+      render :edit
+    end
   end
 
   def show 
@@ -33,7 +47,7 @@ class UsersController < ApplicationController
   private 
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, roles_attributes: [:id])
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, role_ids:[])
   end
 
   def set_user
