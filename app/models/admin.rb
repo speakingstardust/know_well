4e2cfcf24a9d2e005d889d4d44525867ebc5ac64
speakingstardust <josh@autochemky.com>
2017-02-24 09:38:01 -0500

    created policy for jigs, next to authorize in JigController

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,4 +2,9 @@ class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :trackable, :validatable, :timeoutable
+
+
+  def is_admin?
+    true
+  end
 end
