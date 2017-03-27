cb6f1c3e4bc9e26e385438ca344a5e5b77670909
speakingstardust <josh@autochemky.com>
2017-03-27 11:10:12 -0400

    Changed email notification for jig work orders being created and verified, now to test.

--- app/mailers/jig_work_order_mailer.rb
+++ app/mailers/jig_work_order_mailer.rb
@@ -6,4 +6,10 @@ class JigWorkOrderMailer < ApplicationMailer
     @jig_work_order = jig_work_order
     mail to: @user.email, subject: "New Jig Work Order for #{@jig_work_order.customer.name}"
   end 
+
+  def jig_work_order_verified(user, jig_work_order)
+    @user = user
+    @jig_work_order = jig_work_order
+    mail to: @user.email, subject: "Jig Work Order for #{@jig_work_order.customer.name} Verified"
+  end
 end
