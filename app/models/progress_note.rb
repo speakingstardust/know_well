fa41a0a029e462b3ae881c837feb6f667958870a
speakingstardust <josh@autochemky.com>
2017-04-12 10:16:24 -0400

    Setup forms and model for progress_note attachment but broke form due to use of AJAX, now to fix so that AJAX method continues to work.

--- app/models/progress_note.rb
+++ app/models/progress_note.rb
@@ -1,3 +1,8 @@
 class ProgressNote < ActiveRecord::Base
   belongs_to :project
+
+  has_attached_file :attachment, styles: { thumb: '100x100>', medium: '300x300>' }
+
+  validates_attachment_content_type :attachment, :content_type => ["application/pdf","application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "text/plain", "image/jpeg", "image/jpg", "image/png", "application/zip", "application/x-iwork-keynote-sffkey", "application/x-iwork-pages-sffpages", "application/x-iwork-numbers-sffnumbers"]
+
 end
