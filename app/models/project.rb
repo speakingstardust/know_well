6739c1f098c5c9f832b1e687fc5b71dcce916cfe
speakingstardust <josh@autochemky.com>
2017-04-12 15:29:28 -0400

    Started setting up project method to create gantt chart, need to setup a self referential relationship between tasks.

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,4 +8,28 @@ class Project < ActiveRecord::Base
   enum status: [:active, :urgent, :low, :suspended, :canceled, :completed]
   enum department: [:lab, :sales, :it, :production]
 
+  def generate_gantt_chart
+    data_table = GoogleVisualr::DataTable.new
+    
+    data_table.new_column('string', 'Task ID')
+    data_table.new_column('string', 'Task Name')
+    data_table.new_column('date', 'Start Date')
+    data_table.new_column('date', 'End Date')
+    data_table.new_column('number', 'Duration')
+    data_table.new_column('number', 'Percent Complete')
+    data_table.new_column('string', 'Dependencies')
+
+    rows = []
+    self.tasks.each do |task|
+      row = []
+      row << task.name
+      row << task.description
+      row << task.start_date
+      row << task.end_date
+      row << task.duration
+      row << task.percent_complete
+      row << task.percent_complete
+    end
+  end
+
 end
