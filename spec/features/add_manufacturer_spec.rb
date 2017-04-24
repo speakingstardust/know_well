71d7955702b065d29fa4e1f08fe00be53566c92e
speakingstardust <josh@autochemky.com>
2017-04-24 13:54:18 -0400

    Setup Manufacturer's index page

--- spec/features/add_manufacturer_spec.rb
+++ spec/features/add_manufacturer_spec.rb
@@ -15,10 +15,10 @@ RSpec.describe "Manufacturer Management", type: :feature do
       fill_in "Website", with: "www.testmanufacturer.com"
       fill_in "Phone", with: "555-555-5555"
       click_on "Create Manufacturer" 
-      @manufacturer = Manufacturer.where(name: "Test Manufacturer").first
+      @manufacturer = Manufacturer.find_by_name("Test Manufacturer")
       binding.pry
 
-      expect(page).to have_current_path(manufacturer_path, only_path: true)
+      expect(page).to have_current_path(manufacturer_path(@manufacturer))
 
       visit manufacturers_path
       expect(page).to have_selector("#manufacturer_#{@manufacturer.id} .name", text: "Test Manufacturer") 
