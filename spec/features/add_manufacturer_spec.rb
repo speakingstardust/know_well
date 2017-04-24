32c197e4b9bd5b4077637fcb38e1a0dc044320f9
speakingstardust <josh@autochemky.com>
2017-04-24 14:06:43 -0400

    Fixed create manufacturer feature spec so that it is properly working

--- spec/features/add_manufacturer_spec.rb
+++ spec/features/add_manufacturer_spec.rb
@@ -16,14 +16,13 @@ RSpec.describe "Manufacturer Management", type: :feature do
       fill_in "Phone", with: "555-555-5555"
       click_on "Create Manufacturer" 
       @manufacturer = Manufacturer.find_by_name("Test Manufacturer")
-      binding.pry
 
       expect(page).to have_current_path(manufacturer_path(@manufacturer))
 
       visit manufacturers_path
-      expect(page).to have_selector("#manufacturer_#{@manufacturer.id} .name", text: "Test Manufacturer") 
-      expect(page).to have_selector("#manufacturer_#{@manufacturer.id} .website", text: "www.testmanufacturer.com")
-      expect(page).to have_selector("#manufacturer_#{@manufacturer.id} .phone", text: "555-555-5555") 
+      expect(page).to have_content("Test Manufacturer")
+      expect(page).to have_css("a", text: "Test Manufacturer's Website")
+      expect(page).to have_content("555-555-5555")
     end
   end
 end
