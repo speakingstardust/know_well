6319eb8927952ebe3c1b4a12a855b737fdb9dc61
speakingstardust <josh@autochemky.com>
2017-04-24 10:41:25 -0400

    Setup Manufacturers controller actions

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -1,13 +1,50 @@
 class ManufacturersController < ApplicationController
+  before_action :set_manufacturer, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_any!
+
   def index
+    @manufacturers = Manufacturer.all
   end
 
   def new
+    @manufacturer = Manufacturer.new
+  end
+
+  def create
+    @manufacturer = Manufacturer.new(manufacturer_params)
+
+    if @manufacturer.save
+      redirect_to @manufacturer, notice: "Manufacturer was successfully created."
+    else 
+      render :new
+    end
   end
 
   def edit
   end
 
+  def update
+    if @manufacturer.update(manufacturer_params)
+      redirect_to @manufacturer, notice: "Manufacturer was successfully updated."
+    else
+      render :edit
+    end
+  end
+
   def show
   end
+
+  def destroy
+    @manufacturer.destroy
+    redirect_to manufacturers_url, notice: "Manfacturer was succefully destroyed."
+  end
+
+  private 
+    def manufacturer_params
+      params.require(:manufacturer).permit(:name, :website, :phone)
+    end
+
+    def set_manufacturer
+      @manufacturer = Manufacturer.find(params[:id])
+    end
 end
