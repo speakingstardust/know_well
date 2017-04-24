d01a5ae44e6ec0b41ee95a4c834f9bf99cf9b353
speakingstardust <josh@autochemky.com>
2017-04-24 11:05:23 -0400

    Create test for name, and name uniqueness

--- spec/models/manufacturer_spec.rb
+++ spec/models/manufacturer_spec.rb
@@ -7,9 +7,18 @@ RSpec.describe Manufacturer, type: :model do
     it "can have a name set" do 
       manufacturer.name = "Test Manufacturer Name" 
 
-      manufacturer.save
+      expect(manufacturer).to be_valid
+    end
 
-      expect(manufacturer.name).to eq("Test Manufacturer Name")
+    it "is not valid without a name" do 
+      expect(manufacturer).to_not be_valid
+    end 
+
+    it "is not valid without a unique name" do 
+      second_manufacturer = Manufacturer.create(name: "Test Manufacturer") 
+      manufacturer.name = "Test Manufacturer" 
+
+      expect(manufacturer).to_not be_valid
     end
   end 
 end
