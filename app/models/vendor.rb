177a8fd7614e906928f9fca3e06c9b737b7824ad
speakingstardust <josh@autochemky.com>
2017-04-25 12:32:16 -0400

    Add validations to vendor model and specs for validations

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,2 +1,5 @@
 class Vendor < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validates :phone, presence: true
+  validates :website, presence: true
 end
--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Vendor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do 
+    let (:vendor) { Vendor.new }
+
+    it "is valid with valid attributes" do 
+      second_vendor = Vendor.create(name: "Unique Vendor")
+      vendor.name = "Test Vendor" 
+      vendor.phone = "555-555-5555"
+      vendor.website = "www.google.com"
+
+      expect(vendor).to be_valid
+    end
+
+    it "is not valid without a name" do 
+      vendor.phone = "555-555-5555"
+      vendor.website = "www.google.com"
+
+      expect(vendor).to_not be_valid
+    end
+
+    it "is not valid without a unique name" do 
+      second_vendor = Vendor.create(name: "Test Vendor")
+      vendor.name = "Test Vendor" 
+
+      expect(vendor).to_not be_valid
+    end
+
+    it "is not valid without a phone number" do 
+      vendor.name = "Test Vendor"
+      vendor.website = "www.google.com"
+
+      expect(vendor).to_not be_valid
+    end
+
+    it "is not valid without a website" do 
+      vendor.name = "Test Vendor" 
+      vendor.phone = "555-555-5555"
+
+      expect(vendor).to_not be_valid
+    end
+  end
 end
