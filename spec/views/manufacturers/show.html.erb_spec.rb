f0219d78d3a27ae24307294b0208788c586d8630
speakingstardust <josh@autochemky.com>
2017-04-25 13:08:15 -0400

    Remove autogenerated specs for helper and view tests

--- spec/views/manufacturers/show.html.erb_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "manufacturers/show.html.erb", type: :view do
-end
--- spec/views/projects/new.html.erb_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "projects/new.html.erb", type: :view do
-end
