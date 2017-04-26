bd13b2033d170d21c719e3e536b1e4fb86f7b0b2
speakingstardust <josh@autochemky.com>
2017-04-26 10:12:24 -0400

    Setup validation specs for product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do 
+    it "is valid with valid attributes"
+    it "is not valid without a name" 
+    it "is not valid without a unique name" 
+    it "is not valid without a unit" 
+    it "is not valid without a container"
+    it "is not valid without units per container"
+    it "is not valid without either a price per unit or a price per container"
+    it "is not valid without a maximum on hand" 
+    it "is not valid without a minimum on hand"
+    it "is not valid without a category"
+  end
 end
