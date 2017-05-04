48cb6fff2c70a1379b236912fb25e07c4e260a2e
speakingstardust <josh@autochemky.com>
2017-05-04 14:18:41 -0400

    Setup initial spec for validations

--- spec/models/order_line_item_spec.rb
+++ spec/models/order_line_item_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe OrderLineItem, type: :model do
+  describe "Validations" do
+    let(:order_line_item) { OrderLineItem.new } 
+    
+    it "is valid with valid attributes" do 
+      expect(order_line_item).to be_valid
+    end
+    it "is not valid without a product" 
+    it "is not valid without an amount required" 
+  end
 end
