4fdce430fc640a45b3d6ec0cbff4591258350e5d
speakingstardust <josh@autochemky.com>
2017-05-09 11:05:59 -0400

    Setup validations spec for jig work orders

--- spec/models/jig_work_order_spec.rb
+++ spec/models/jig_work_order_spec.rb
@@ -1,4 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe JigWorkOrder, type: :model do
+  describe "Validations" do 
+    let(:jig) { create(:jig) } 
+    let(:jig_work_order_line_item) { JigWorkOrderLineItem.new(jig: jig, expected: 5, repaired: 0, cleaned: 5) }
+    let(:return_date) { Date.today }
+    let(:pickup_date) { Date.today - 3 }
+    subject { described_class.new(jig_work_order_line_items: [jig_work_order_line_item], 
+                                  purchase_order: "12345", notes: "Testing notes", 
+                                  returned: return_date, pickup_date: pickup_date, 
+                                  pickup_time: "AM", return_time: "PM") } 
+
+    it "should be valid with valid attributes" do 
+      expect(subject).to be_valid
+    end
+
+    it "should not be valid without a pickup date" do
+      subject.pickup_date = nil 
+
+      expect(subject).to_not be_valid
+    end
+  end
 end
