60a958e523e3aa8bbccfa258d283c967e81f3d11
speakingstardust <josh@autochemky.com>
2017-05-09 10:56:07 -0400

    Setup basic validations spec for jig work order line items

--- spec/models/jig_work_order_line_item_spec.rb
+++ spec/models/jig_work_order_line_item_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe JigWorkOrderLineItem, type: :model do
+  describe "Validations" do 
+    let(:jig) { create(:jig) }
+    subject { described_class.new(jig: jig, expected: 5, repaired: 2, actual: 6) }
+
+    it "should be valid with valid attributes" do 
+      expect(subject).to be_valid
+    end
+
+    it "should not be valid without a jig" do 
+      subject.jig = nil
+
+      expect(subject).to_not be_valid
+    end
+  end
 end
