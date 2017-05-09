83ce784dece3780115486ad9664fd23528b9c4cd
speakingstardust <josh@autochemky.com>
2017-05-09 16:19:02 -0400

    Setup jig work order create feature

---
+++ spec/features/jig_work_order_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+require 'pry'
+
+RSpec.describe "Jig Work Order Management", type: :feature do 
+
+  def login_shop_supervisor
+    shop_supervisor = FactoryGirl.create(:user)
+    shop_supervisor.add_role(:shop_supervisor)
+    login_as(shop_supervisor, :scope => :user)
+  end
+
+  def login_bookkeeper
+    bookkeeper = FactoryGirl.create(:user) 
+    bookkeeper.add_role(:bookkeeper)
+    login_as(bookkeeper, :scope => :user)
+  end
+
+  describe "Create" do 
+    it "allows a bookkeeper to create a Jig Work Order" do 
+      @customer = FactoryGirl.create(:customer) 
+      @jig = FactoryGirl.create(:jig, customer: @customer) 
+
+      login_bookkeeper
+
+      visit work_orders_management_path
+      select @customer, from: "Customer" 
+      click_on "Jig Work Order"
+
+      expect(page).to have_current_path(new_jig_work_order_path)
+
+      select Date.today.year, from: "jig_work_order_pickup_date_1i"
+      select Date.today.month, from: "jig_work_order_pickup_date_21"
+      select Date.today.day, from: "jig_work_order_pickup_date_3i"
+      fill_in "Pickup Time", with: "AM"
+      fill_in "Notes", with: "Test Jig Work Order notes"
+      select @jig, from: "jig_work_order_jig_work_order_line_items_attributes_0_jig_id"
+      fill_in "jig_work_order_jig_work_order_line_items_attributes_0_expected", with: 5
+      
+      expect {
+        click_on "Create Jig work order" 
+      }.to change(JigWorkOrder, :count).by(1)
+
+      @jig_work_order = JigWorkOrder.first
+
+      expect(page).to have_current_path(jig_work_order_path(@jig_work_order))
+      expect(page).to have_content(@jig.name)
+      expect(page).to have_content(5)
+      expect(page).to have_content(@customer.name)
+      expect(page).to have_content(@jig_work_order.id)
+      expect(page).to have_content(Date.today)
+    end
+  end
+end
