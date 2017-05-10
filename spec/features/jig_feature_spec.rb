bab5d4b67154ed0290c1792de0b17f6efbf47f8b
speakingstardust <josh@autochemky.com>
2017-05-10 10:17:26 -0400

    Setup bookkeeper create jig feature spec

--- spec/features/jig_feature_spec.rb
+++ spec/features/jig_feature_spec.rb
@@ -36,5 +36,27 @@ RSpec.describe "Jig Management", type: :feature do
       expect(page).to have_content(@jig.name)
       expect(page).to have_content(@customer.name)
     end
+
+    it "allows a bookkeeper to create a jig with prices" do 
+      login_bookkeeper
+
+      visit new_jig_path
+
+      fill_in "Name", with: "Test Jig" 
+      select @customer.name, from: "Customer" 
+      fill_in "Cleaning charge", with: 9.50
+      fill_in "Repair charge", with: 6.50
+      expect {
+        click_on "Create Jig"
+      }.to change(Jig, :count).by(1)
+
+      @jig = Jig.find_by_name("Test Jig")
+
+      expect(page).to have_current_path(jig_path(@jig))
+      expect(page).to have_content(@jig.name)
+      expect(page).to have_content(@customer.name)
+      expect(page).to have_content(@jig.cleaning_charge)
+      expect(page).to have_content(@jig.repair_charge)
+    end 
   end 
 end
