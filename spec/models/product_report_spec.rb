0c9e15e6a8d7373ad42492828c076da38b27d40a
speakingstardust <josh@autochemky.com>
2017-05-17 12:48:01 -0400

    Create spec for product report setting products logic

--- spec/models/product_report_spec.rb
+++ spec/models/product_report_spec.rb
@@ -18,4 +18,16 @@ RSpec.describe ProductReport, type: :model do
     it { should have_many(:product_report_line_items) }
     it { should have_many(:products) }
   end
+  
+  describe "Logic" do 
+    let!(:raw_material) { FactoryGirl.create(:product, category: :raw_materials) }
+    let!(:consumable) { FactoryGirl.create(:product, category: :consumables) }
+    let!(:product_report) { FactoryGirl.create(:product_report) }
+
+    it "can find all products when given a set of categories" do 
+      product_report.find_products([:raw_materials, :consumables])
+
+      expect(product_report.products).to include(raw_material, consumable) 
+    end
+  end
 end
