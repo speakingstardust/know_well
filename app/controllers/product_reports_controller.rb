a0aaa3406d61880a430064b3fe4fb9fe49c98dd3
speakingstardust <josh@autochemky.com>
2017-05-17 14:01:10 -0400

    Setup beginning of product report controller

--- app/controllers/product_reports_controller.rb
+++ app/controllers/product_reports_controller.rb
@@ -1,10 +1,25 @@
 class ProductReportsController < ApplicationController
+  before_action :authenticate_any! 
   def index
+    @product_reports = ProductReport.all
   end
 
   def show
+    @product_report = ProductReport.find(params[:id])
   end
 
   def new
+    @product_report = ProductReport.new
+    @categories = Product.categories
   end
+  
+  def create
+
+  end
+
+  private 
+
+    def product_report_params
+      params.require(:product_report).permit(:categories, :notes)
+    end
 end
