4445fd115250ac0533927130b5df6c583d236737
speakingstardust <josh@autochemky.com>
2017-05-17 12:02:16 -0400

    Setup date created validations and before_save callback

--- app/models/product_report.rb
+++ app/models/product_report.rb
@@ -1,2 +1,12 @@
 class ProductReport < ActiveRecord::Base
+  has_many :product_report_line_items, dependent: :destroy
+  has_many :products, through: :product_report_line_items
+
+  before_save :set_date_created
+
+  validates :date_created, presence: true
+
+  def set_date_created
+    self.date_created = Date.today
+  end
 end
