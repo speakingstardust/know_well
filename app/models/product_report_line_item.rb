42e77abeef2a46305c0892722337c8d08f2ff0dd
speakingstardust <josh@autochemky.com>
2017-05-17 12:14:04 -0400

    Add validations to product_report_line_item model

--- app/models/product_report_line_item.rb
+++ app/models/product_report_line_item.rb
@@ -1,4 +1,8 @@
 class ProductReportLineItem < ActiveRecord::Base
   belongs_to :product_report
   belongs_to :product
+
+  validates :product_report, presence: true
+  validates :product, presence: true
+  validates :current_amount, presence: true
 end
