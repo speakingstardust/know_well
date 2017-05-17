97d6766ef3590e970e6775a2c3af590a9ed35515
speakingstardust <josh@autochemky.com>
2017-05-17 12:11:39 -0400

    Setup basic product_report_line_item model validation specs

--- spec/models/product_report_line_item_spec.rb
+++ spec/models/product_report_line_item_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe ProductReportLineItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do 
+    let(:product) { FactoryGirl.create(:product) } 
+    let(:product_report) { FactoryGirl.create(:product_report) } 
+    subject { described_class.new(product: product, 
+                                  current_amount: product.curren_on_hand, 
+                                  product_report: product_report) }
+
+    it "should be valid with valid attributes" do 
+      expect(subject).to be_valid
+    end
+
+    it "should not be valid without a product report" do 
+      subject.product_report = nil 
+      expect(subject).to_not be_valid
+    end
+
+    it "should not be valid without a product" do 
+      subject.product = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "should not be valid without a current amount" do 
+      subject.current_amount = nil 
+      expect(subject).to_not be_valid
+    end
+  end
 end
