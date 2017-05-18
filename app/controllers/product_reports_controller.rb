493716544e2a9f14dad2b51f515622875089c68c
speakingstardust <josh@autochemky.com>
2017-05-18 15:14:29 -0400

    Setup product_report controller create action and show view

--- app/controllers/product_reports_controller.rb
+++ app/controllers/product_reports_controller.rb
@@ -14,12 +14,25 @@ class ProductReportsController < ApplicationController
   end
   
   def create
-
+    parse_params
+    @notes = params[:product_report][:notes]
+    @action = CreatesProductReport.new(@categories, @notes) 
+    success = @action.create
+    if success
+      redirect_to product_report_path(@action.product_report)
+    else
+      @action.product_report.destroy
+      redirect_to new_product_report_url
+      flash[:error] = "Product Report not generated, a problem has occured."
+    end
   end
 
   private 
+    def parse_params
+      @categories = Category.where(id: params[:product_report][:category_ids])
+    end
 
     def product_report_params
-      params.require(:product_report).permit(:categories, :notes)
+      params.require(:product_report).permit(:categories, :notes, :category_ids)
     end
 end
