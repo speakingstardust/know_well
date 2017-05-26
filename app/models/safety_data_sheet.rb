6126ff1c6c521aa935808a0ffa1c588bd8fb8a43
speakingstardust <josh@autochemky.com>
2017-05-26 11:21:16 -0400

    Add validation for manufacturer's name to sds

--- app/models/safety_data_sheet.rb
+++ app/models/safety_data_sheet.rb
@@ -2,4 +2,5 @@ class SafetyDataSheet < ActiveRecord::Base
   enum category: [:MEICO_products, :used_in_house, :raw_materials]
 
   validates :product_name, presence: true, uniqueness: true
+  validates :manufacturer_name, presence: true
 end
