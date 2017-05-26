abf909885df47f759c1c053e2b1f0f5ee0905eb4
speakingstardust <josh@autochemky.com>
2017-05-26 11:27:41 -0400

    Ran migration for paperclip attachment for sds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170526150924) do
+ActiveRecord::Schema.define(version: 20170526152614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -296,6 +296,10 @@ ActiveRecord::Schema.define(version: 20170526150924) do
     t.integer  "category"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.string   "pdf_file_name"
+    t.string   "pdf_content_type"
+    t.integer  "pdf_file_size"
+    t.datetime "pdf_updated_at"
   end
 
   create_table "signatures", force: :cascade do |t|
