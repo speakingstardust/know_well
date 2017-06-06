d17f9c93f18beb9bc95f8748e72ae466929c17e2
speakingstardust <josh@autochemky.com>
2017-06-06 11:29:01 -0400

    Fix typo

--- spec/models/safety_data_sheet_spec.rb
+++ spec/models/safety_data_sheet_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe SafetyDataSheet, type: :model do
   describe "Validations" do 
     subject { described_class.new(product_name: "Test Product", manufacturer_name: "Test Manufacturer", 
                                   category: 0, 
-                                  pdf: File.new("#{Rails.root}/spec/support/fixtures/SDS_Grate.pdf") } 
+                                  pdf: File.new("#{Rails.root}/spec/support/fixtures/SDS_Grate.pdf")) } 
 
     it "is valid with valid attributes" do 
       expect(subject).to be_valid
