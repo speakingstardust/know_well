f7ce77abf31d57d6d08846923e18e5a2eb564022
speakingstardust <josh@autochemky.com>
2017-06-06 12:50:45 -0400

    Setup initial edit sds feature spec

--- spec/features/safety_data_sheet_spec.rb
+++ spec/features/safety_data_sheet_spec.rb
@@ -29,4 +29,20 @@ RSpec.describe "SDS Management", type: :feature do
       expect(page).to have_content("Test Manufacturer")
     end
   end
+  describe "Edit" do 
+    let(:sds) { create(:safety_data_sheet) }
+    it "allows a user to edit an existing SDS" do 
+      visit edit_safety_data_sheet_path(sds)
+
+      fill_in "Product name", with: "Different Product Name" 
+      fill_in "Manufacturer name", with: "Different Manufacturer Name" 
+      select "MEICO Products", from: "Category"
+      click_on "Update Safety data sheet"
+
+      expect(page).to have_current_path(safety_data_sheet_path(sds))
+      expect(page).to have_content("Different Product Name") 
+      expect(page).to have_content("Different Manufacturer Name") 
+      expect(page).to have_content("MEICO Products")
+    end
+  end
 end
