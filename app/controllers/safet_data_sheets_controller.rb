3f147edd1d25b507f3b73f56863757d9e24b14ed
speakingstardust <josh@autochemky.com>
2017-06-06 11:46:26 -0400

    Generate SDS Controller

---
+++ app/controllers/safet_data_sheets_controller.rb
@@ -0,0 +1,13 @@
+class SafetDataSheetsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
---
+++ app/helpers/safet_data_sheets_helper.rb
@@ -0,0 +1,2 @@
+module SafetDataSheetsHelper
+end
