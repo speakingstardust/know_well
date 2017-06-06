15592f5338b5b130ac7ea92edcfb8045485b1d4a
speakingstardust <josh@autochemky.com>
2017-06-06 12:53:38 -0400

    Fix typo in feature spec for sds

--- spec/features/safety_data_sheet_spec.rb
+++ spec/features/safety_data_sheet_spec.rb
@@ -36,13 +36,13 @@ RSpec.describe "SDS Management", type: :feature do
 
       fill_in "Product name", with: "Different Product Name" 
       fill_in "Manufacturer name", with: "Different Manufacturer Name" 
-      select "MEICO Products", from: "Category"
+      select "Meico Products", from: "Category"
       click_on "Update Safety data sheet"
 
       expect(page).to have_current_path(safety_data_sheet_path(sds))
       expect(page).to have_content("Different Product Name") 
       expect(page).to have_content("Different Manufacturer Name") 
-      expect(page).to have_content("MEICO Products")
+      expect(page).to have_content("Meico Products")
     end
   end
 end
