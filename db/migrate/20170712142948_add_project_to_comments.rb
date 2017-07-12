463bbac87ce40a4a9c4c4a8e1d18939f2360c940
speakingstardust <josh@autochemky.com>
2017-07-12 10:32:24 -0400

    Setup migration for adding project references to comment model

--- db/migrate/20170712142948_add_project_to_comments.rb
+++ db/migrate/20170712142948_add_project_to_comments.rb
@@ -1,4 +1,8 @@
 class AddProjectToComments < ActiveRecord::Migration
   def change
+    change_table :comments do |t|
+      t.references :project
+    end 
+    add_index :comments, :project_id
   end
 end
