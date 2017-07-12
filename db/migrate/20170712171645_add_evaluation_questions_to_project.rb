6686f76f161aa655e476d56c9f26b0d075e78bb5
speakingstardust <josh@autochemky.com>
2017-07-12 13:22:42 -0400

    Setup migration with text fields for all of the project evaluation questions

--- db/migrate/20170712171645_add_evaluation_questions_to_project.rb
+++ db/migrate/20170712171645_add_evaluation_questions_to_project.rb
@@ -1,4 +1,15 @@
 class AddEvaluationQuestionsToProject < ActiveRecord::Migration
   def change
+    change_table :projects do |t|
+      t.text :why_question
+      t.text :market_question
+      t.text :current_situation_question
+      t.text :problems_question
+      t.text :outcome_question
+      t.text :customer_agreed_question
+      t.text :requirements_question
+      t.text :must_do_question
+      t.text :must_not_do_question
+    end
   end
 end
