04e5728f5eaea8921dbc338edd0a5e2f4708f117
speakingstardust <josh@autochemky.com>
2017-07-12 10:32:49 -0400

    Ran migration to create comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627163231) do
+ActiveRecord::Schema.define(version: 20170712142948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,17 @@ ActiveRecord::Schema.define(version: 20170627163231) do
 
   add_index "categories", ["product_report_id"], name: "index_categories_on_product_report_id", using: :btree
 
+  create_table "comments", force: :cascade do |t|
+    t.string   "creator_name"
+    t.string   "subject"
+    t.text     "comment"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "project_id"
+  end
+
+  add_index "comments", ["project_id"], name: "index_comments_on_project_id", using: :btree
+
   create_table "customers", force: :cascade do |t|
     t.string   "name"
     t.string   "phone"
@@ -60,6 +71,14 @@ ActiveRecord::Schema.define(version: 20170627163231) do
 
   add_index "dependencies", ["dependent_task_id", "task_id"], name: "index_dependencies_on_dependent_task_id_and_task_id", unique: true, using: :btree
   add_index "dependencies", ["task_id", "dependent_task_id"], name: "index_dependencies_on_task_id_and_dependent_task_id", unique: true, using: :btree
+
+  create_table "incident_types", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "recordable"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "jig_order_line_items", force: :cascade do |t|
     t.integer  "repaired"
