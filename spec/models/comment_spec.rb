7035d711630688b90275319acbcaf0b3d454347c
speakingstardust <josh@autochemky.com>
2017-07-12 10:38:45 -0400

    Setup basic validation spec for comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do 
+    subject { described_class.new(creator_name: "Test Creator", subject: "Test Comment Subject",
+                                  comment: "Test comment for the project tracking system")
+    } 
+
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a creator name" do 
+      subject.creator_name = nil 
+
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a subject" do 
+      subject.subject = nil 
+
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a comment" do 
+      subject.comment = nil 
+
+      expect(subject).to_not be_valid
+    end
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,10 +3,9 @@ require 'pry'
 
 RSpec.describe Order, type: :model do
   describe "Validations" do 
-    let(:order) { FactoryGirl.create(:order) } 
+    let!(:order) { FactoryGirl.create(:order) } 
 
     it "is valid with valid attributes" do
-      order 
       expect(order).to be_valid
     end
   end
