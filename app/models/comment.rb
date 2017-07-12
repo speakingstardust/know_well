98c3ed597db04090355a7d73aca84feb9b112bfc
speakingstardust <josh@autochemky.com>
2017-07-12 10:57:40 -0400

    Add logic to set creator name to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,13 @@
 class Comment < ActiveRecord::Base
   belongs_to :project
 
+  before_save :set_creator_name
+
   validates :creator_name, presence: true 
   validates :subject, presence: true
   validates :comment, presence: true
+
+  def set_creator_name
+    self.creator_name = pundit_user.full_name
+  end
 end
