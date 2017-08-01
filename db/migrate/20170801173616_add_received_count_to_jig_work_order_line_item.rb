2a248c4ff2de439e0d9aae156ba1ec1d42a5e5d6
speakingstardust <josh@autochemky.com>
2017-08-01 13:40:03 -0400

    Setup migration for adding received count

--- db/migrate/20170801173616_add_received_count_to_jig_work_order_line_item.rb
+++ db/migrate/20170801173616_add_received_count_to_jig_work_order_line_item.rb
@@ -1,4 +1,7 @@
 class AddReceivedCountToJigWorkOrderLineItem < ActiveRecord::Migration
   def change
+    change_table :jig_work_order_line_items do |t|
+      t.number :received
+    end
   end
 end
