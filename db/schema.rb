97266eb39b40cfc807a6362dee01a7a5acf64e3a
speakingstardust <josh@autochemky.com>
2017-08-08 11:30:26 -0400

    Run migration to add location to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170802144056) do
+ActiveRecord::Schema.define(version: 20170808152838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -235,6 +235,7 @@ ActiveRecord::Schema.define(version: 20170802144056) do
     t.datetime "updated_at",          null: false
     t.integer  "category_id"
     t.string   "serial_number"
+    t.string   "location"
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
