ff698a1fcbc249b7d0e3cd821185d43187d1d5fe
speakingstardust <josh@autochemky.com>
2017-08-22 14:13:00 -0400

    Fix create expense report feature spec date selects

--- spec/features/expense_report_feature_spec.rb
+++ spec/features/expense_report_feature_spec.rb
@@ -16,7 +16,9 @@ RSpec.describe "Expense Report Management", type: :feature do
       select "Other", from: "Category"
       fill_in "Note", with: "Test Note"
       fill_in "Other note", with: "Test Other Note"
-      select Date.today, from: "Date"
+      select Date.today.year, from: "expense_report_date_1i"
+      select Date.today.month, from: "expense_report_date_2i"
+      select Date.today.day, from: "expense_report_date_3i"
       expect {
         click_on "Create Expense report"
       }.to change(ExpenseReport, :count).by(1)
