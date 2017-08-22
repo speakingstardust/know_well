ee96d46b9d7aa5bb67f8b13e04b358e33dcf4119
speakingstardust <josh@autochemky.com>
2017-08-22 14:08:13 -0400

    Setup Create expense report view and add logic for assigning admin or user to expense report

--- app/models/expense_report.rb
+++ app/models/expense_report.rb
@@ -12,4 +12,13 @@ class ExpenseReport < ActiveRecord::Base
   validates :category, presence: true
   validates :other_note, presence: true, if: "self.other?"
 
+  before_create :add_user_or_admin
+
+  def add_user_or_admin
+    if pundit_user.is_admin?
+      self.admin = pundit_user
+    else
+      self.user = pundit_user
+    end
+  end
 end
