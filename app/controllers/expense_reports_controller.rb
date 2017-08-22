8cbf9cf142d940b83ff1c7b1bf73454ba85e88f9
speakingstardust <josh@autochemky.com>
2017-08-22 13:47:56 -0400

    Setup basic expense reports controller actions

--- app/controllers/expense_reports_controller.rb
+++ app/controllers/expense_reports_controller.rb
@@ -1,8 +1,23 @@
 class ExpenseReportsController < ApplicationController
+  before_action :set_expense_report, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_any!
+  
   def index
+    @expense_reports = ExpenseReport.all
   end
 
   def new
+    @expense_report = ExpenseReport.new
+  end
+
+  def create
+    @expense_report = ExpenseReport.new(expense_report_params)
+
+    if @expense_report.save 
+      redirect_to @expense_report, notice: "Expense Report was successfully created."
+    else
+      render :new
+    end
   end
 
   def show
@@ -10,4 +25,27 @@ class ExpenseReportsController < ApplicationController
 
   def edit
   end
+
+  def update 
+    if @expense_report.update(expense_report_params)
+      redirect_to @expense_report, notice: "Expense Report was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @expense_report.destroy
+    redirect_to expense_reports_url, notice: "Expense Report was successfully destroyed."
+  end
+
+  private 
+    def set_expense_report
+      @expense_report = ExpenseReport.find(params[:id])
+    end
+
+    def expense_report_params
+      params.require(:expense_report).permit(:user_id, :admin_id, :date, :amount, :vendor, :category,
+                                             :note, :other_note)
+    end
 end
