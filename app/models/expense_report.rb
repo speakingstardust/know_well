396be99213c83a6c67ecf603aac7581eecb575a8
speakingstardust <josh@autochemky.com>
2017-08-24 13:10:30 -0400

    Setup enum for payment method and added validation for payment method on expense reports

--- app/models/expense_report.rb
+++ app/models/expense_report.rb
@@ -4,6 +4,7 @@ class ExpenseReport < ActiveRecord::Base
   belongs_to :admin
 
   enum category: [:shop_fuel, :shop_supplies, :vehicle_fuel, :office_supplies, :other]
+  enum payment_method: [:fuel, :visa, :mastercard, :lowes, :employee_reimbursement]
 
   validates :user, presence: true, if: "admin_id.nil?"
   validates :admin, presence: true, if: "user_id.nil?"
@@ -11,6 +12,7 @@ class ExpenseReport < ActiveRecord::Base
   validates :amount, presence: true
   validates :vendor, presence: true
   validates :category, presence: true
+  validates :payment_method, presence: true
   validates :other_note, presence: true, if: "self.other?"
 
 end
