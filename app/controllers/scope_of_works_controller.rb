d9710a114ccfa574712c8c48fdb513e91c17d53f
speakingstardust <josh@autochemky.com>
2017-09-14 14:21:05 -0400

    Setup basic controller actions for scope of work

--- app/controllers/scope_of_works_controller.rb
+++ app/controllers/scope_of_works_controller.rb
@@ -1,13 +1,51 @@
 class ScopeOfWorksController < ApplicationController
+  before_action :set_scope_of_work, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_any!
+
   def index
+    @scope_of_works = ScopeOfWork.all
+  end
+
+  def new 
+    @scope_of_work = ScopeOfWork.new
+    @customers = Customer.all
+  end
+
+  def create 
+    @scope_of_work = ScopeOfWork.new(scope_of_work_params)
+    
+    if @scope_of_work.save
+      redirect_to @scope_of_work, notice: "Scope of Work was successfully created."
+    else
+      render :new
+    end
   end
 
   def show
   end
 
-  def new
-  end
-
   def edit
   end
+
+  def update 
+    if @scope_of_work.update(scope_of_work_params)
+      redirect_to @scope_of_work, notice: "Scope of Work was succesfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy 
+    @scope_of_work.destroy
+    redirect_to scope_of_works_url, notice: "Scope of Work was successfully destroyed."
+  end
+
+  private 
+    def set_scope_of_work
+      @scope_of_work = ScopeOfWork.find(params[:id])
+    end
+
+    def scope_of_work_params
+      params.require(:scope_of_work).permit(:name, :date_created, :created_by, :description, :file)
+    end
 end
