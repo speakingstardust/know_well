490aa3938729c8846b648284b4b0009c13d72e4b
speakingstardust <josh@autochemky.com>
2017-11-08 11:23:29 -0500

    Configured autochem logo to send with shipping notfication email

--- app/mailers/jig_work_order_mailer.rb
+++ app/mailers/jig_work_order_mailer.rb
@@ -16,7 +16,7 @@ class JigWorkOrderMailer < ApplicationMailer
   def jig_work_order_shipped(user, jig_work_order)
     @user = user
     @jig_work_order = jig_work_order
-    attachments.inline['autochem_industrial.png'] = File.read('autochem_industrial.png')
+    attachments.inline['autochem_industrial.png'] = File.read("#{Rails.root}/app/assets/images/autochem_industrial.png")
     mail to: @user.email, subject: "Jig Work Order for #{ @jig_work_order.customer.name } Shipped"
   end
 end
