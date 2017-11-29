44b5eaed1f1b3d5a9e3343c261d79af95949367e
speakingstardust <josh@autochemky.com>
2017-11-29 13:18:59 -0500

    Add validations in order to get validation specs to pass

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,2 +1,8 @@
 class Document < ActiveRecord::Base
+
+  validates :name, presence: true 
+  validates :category, presence: true
+  validates :date_issued, presence: true
+  validates :version, presence: true
+
 end
