7978d42a3bfe8ff4d8f6b09e7304b3d5d67c0cad
speakingstardust <josh@autochemky.com>
2017-12-01 08:51:28 -0500

    Setup CRUD actions on MeicoProducts controller

--- app/controllers/meico_products_controller.rb
+++ app/controllers/meico_products_controller.rb
@@ -1,5 +1,23 @@
 class MeicoProductsController < ApplicationController
+  before_action :authenticate_any!
+  before_action :set_meico_product, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @meico_products = MeicoProduct.all
+  end
+
   def new
+    @meico_product = MeicoProduct.new
+  end
+  
+  def create 
+    @meico_product = MeicoProduct.new(meico_product_params)
+
+    if @meico_product.save
+      redirect_to @meico_product, notice: "MEICO Product successfully created."
+    else 
+      render :new
+    end
   end
 
   def show
@@ -8,6 +26,25 @@ class MeicoProductsController < ApplicationController
   def edit
   end
 
-  def index
+  def update 
+    if @meico_product.update(meico_product_params)
+      redirect_to @meico_product, notice: "MEICO Product was successfully updated."
+    else
+      render :edit
+    end
   end
+
+  def destroy
+    @meico_product.destroy
+    redirect_to meico_products_url, notice: "MEICO Product was successfully destroyed."
+  end
+
+  private
+    def set_meico_product
+      @meico_product = MeicoProduct.find(params[:id])
+    end
+
+    def meico_product_params
+      params.require(:meico_product).permit(:name)
+    end
 end
