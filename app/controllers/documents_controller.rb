a09dc2cf8e54290fb59940d0b0f7d0115b3a0c35
speakingstardust <josh@autochemky.com>
2017-12-01 09:47:26 -0500

    Setup new and create action along with other basic needs on Documents controller

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,2 +1,27 @@
 class DocumentsController < ApplicationController
+  before_action :set_document, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_any!
+
+  def new 
+    @meico_product = MeicoProduct.find(params[:meico_product_id])
+    @document = Document.new(meico_product: @meico_product)
+  end
+
+  def create
+    @document = Document.new(document_params)
+    if @document.save
+      redirect_to meico_product_document_path(@document.meico_product.id, @document), notice: "Document successfully created."
+    else 
+      render :new
+    end
+  end
+
+  private 
+    def set_document 
+      @document = Document.find(params[:id])
+    end
+
+    def document_params
+      params.require(:document).permit(:meico_product_id, :name, :category, :version, :date_issued, :created_by, :updated_by, :file)
+    end
 end
