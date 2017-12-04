fef6a235b3f8007df97c84b55aee0c698cf5fbc1
speakingstardust <josh@autochemky.com>
2017-12-04 09:44:32 -0500

    Edit edit document feature spec so that it tests for updated_by to set properly after update

--- spec/features/document_feature_spec.rb
+++ spec/features/document_feature_spec.rb
@@ -3,13 +3,13 @@ require "pry"
 
 RSpec.describe "MEICO Product Document Management", type: :feature do 
   before(:each) do 
-    user = FactoryGirl.create(:user)
-    login_as(user, :scope => :user)
+    @user = FactoryGirl.create(:user)
+    login_as(@user, :scope => :user)
   end
 
   describe "Edit" do 
     let!(:meico_product) { FactoryGirl.create(:meico_product) }
-    let!(:document) { FactoryGirl.create(:document, meico_product: meico_product) }
+    let!(:document) { FactoryGirl.create(:document, meico_product: meico_product, updated_by: nil) }
     it "allows a user to edit a document" do 
       visit edit_meico_product_document_path(meico_product.id, document.id)
 
@@ -17,8 +17,8 @@ RSpec.describe "MEICO Product Document Management", type: :feature do
       click_on "Update Document"
 
       expect(page).to have_current_path(meico_product_document_path(meico_product.id, document.id))
-      expect(document.updated_by).to_not eq(nil)
       expect(page).to have_content("Edited Test MEICO Product Name")
+      expect(document.updated_by).to eq("#{@user.first_name} #{@user.last_name}")
     end
   end
 end
