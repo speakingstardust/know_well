bba05000fd493c33ba67c08c732f5107b8c5e2f7
speakingstardust <josh@autochemky.com>
2017-12-08 10:34:44 -0500

    Setup validation spec for user and admin on document mail log

--- spec/models/document_mail_log_spec.rb
+++ spec/models/document_mail_log_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe DocumentMailLog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do 
+    subject { described_class.new(user: create(:user), purpose: "Testing email logs.", 
+                                  sent_at: DateTime.now, customer: create(:customer), 
+                                  email: "test@ing.com") }
+
+    it "is valid with valid attributes" do 
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without either a user or an admin" do
+      expect(subject).to be_valid
+
+      subject.user = nil
+      expect(subject).to_not be_valid
+
+      subject.admin = create(:admin)
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a purpose"
+    it "is not valid without a sent_at timestamp"
+    it "is not valid without an email"
+  end
 end
