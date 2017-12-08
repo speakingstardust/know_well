1de9d40aff36a6240e12142ad8dfe882233096c9
speakingstardust <josh@autochemky.com>
2017-12-08 10:36:41 -0500

    Add dynamic validation for admin and user on document mail log

--- app/models/document_mail_log.rb
+++ app/models/document_mail_log.rb
@@ -2,4 +2,7 @@ class DocumentMailLog < ActiveRecord::Base
   belongs_to :user
   belongs_to :admin
   belongs_to :customer
+
+  validates :user, presence: true, if: "admin_id.nil?"
+  validates :admin, presence: true, if: "user_id.nil?"
 end
