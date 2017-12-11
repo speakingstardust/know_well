43c18d9eb132235b233ec86fb303fbd90a7f0ff7
speakingstardust <josh@autochemky.com>
2017-12-11 14:11:00 -0500

    Still can't get current_version logic to work, might need to try a different approach

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,12 @@
+require "pry"
 class Document < ActiveRecord::Base
   belongs_to :meico_product
   has_many :document_mail_logs
+  
+  before_save :set_current_version
+
+  scope :all_except, ->(document) { where.not(id: document) }
+  scope :current_version, ->(document) { where(category: document.category, current_version: true) }
 
   enum category: [:SDS, :TDS, :EDS, :Other]
 
@@ -18,6 +24,22 @@ class Document < ActiveRecord::Base
 
   def share_document(email)
     DocumentMailer.share_document(email, self).deliver_now
+  end
+
+  def set_current_version
+    if self.current_version_changed?
+      if self.current_version == true
+        previous_version = self.meico_product.documents.current_version(self).all_except(self).first
+        if previous_version
+          previous_version.current_version = false
+          previous_version.save
+        end
+      elsif self.current_version == false
+        if self.meico_product.documents.current_version(self).all_except(self).empty?
+          return false
+        end
+      end
+    end
   end
 
 end
