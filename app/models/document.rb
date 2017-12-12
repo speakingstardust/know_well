46a8ce923be26ba13d282fdec23eeead678d72fd
speakingstardust <josh@autochemky.com>
2017-12-12 08:20:30 -0500

    Edit callback in document model for setting current version

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,9 +2,10 @@ require "pry"
 class Document < ActiveRecord::Base
   belongs_to :meico_product
   has_many :document_mail_logs
-  
-  before_save :set_current_version
 
+  before_save :check_for_one_current_version, if: :current_version_false?
+  after_save :set_current_version_to_false, if: :current_version_true?
+  
   scope :all_except, ->(document) { where.not(id: document) }
   scope :current_version, ->(document) { where(category: document.category, current_version: true) }
 
@@ -26,19 +27,23 @@ class Document < ActiveRecord::Base
     DocumentMailer.share_document(email, self).deliver_now
   end
 
-  def set_current_version
-    if self.current_version_changed?
-      if self.current_version == true
-        previous_version = self.meico_product.documents.current_version(self).all_except(self).first
-        if previous_version
-          previous_version.current_version = false
-          previous_version.save
-        end
-      elsif self.current_version == false
-        if self.meico_product.documents.current_version(self).all_except(self).empty?
-          return false
-        end
-      end
+  def current_version_true?
+    self.current_version
+  end
+
+  def current_version_false?
+    self.current_version
+  end
+
+  def set_current_version_to_false
+    self.class.current_version(self).all_except(self).update_all(current_version: false)
+  end
+
+  def check_for_one_current_version
+    if self.current_version(self).all_except(self).empty?
+      return false
+    else
+      return true
     end
   end
 
