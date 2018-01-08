09294ffa2980fac6f8a75ab3e1119b162ee06ecc
speakingstardust <josh@autochemky.com>
2018-01-08 15:11:46 -0500

    Setup contact factory and kind enum on contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,5 @@
 class Contact < ActiveRecord::Base
   belongs_to :customer
+
+  enum kind: [:primary_billing, :secondary_billing, :primary_purchasing, :secondary_purchasing, :primary_production, :secondary_production]
 end
