a9e4d4aafa4f2350ea33674731be3a333c31bb16
speakingstardust <josh@autochemky.com>
2018-01-08 13:12:46 -0500

    Setup customer show to properly display all addresses

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,7 +4,9 @@ class Customer < ActiveRecord::Base
         has_many :reports
         has_many :scope_of_works
         has_many :document_mail_logs
-        has_many :addresses
+        has_many :addresses, dependent: :destroy
+
+        accepts_nested_attributes_for :addresses, allow_destroy: true
 
 	validates :name, presence: true, uniqueness: true
 
