c449ec922a38ab6207b4a823f146d45c08565469
speakingstardust <josh@autochemky.com>
2018-01-08 15:18:24 -0500

    Setup contact model validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,11 @@
 class Contact < ActiveRecord::Base
   belongs_to :customer
 
+  validates :customer, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone, presence: true
+  validates :email, presence: true
+
   enum kind: [:primary_billing, :secondary_billing, :primary_purchasing, :secondary_purchasing, :primary_production, :secondary_production]
 end
