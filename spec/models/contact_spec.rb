22ff78129bde26aa7e3f851d023d7206ca6d2185
speakingstardust <josh@autochemky.com>
2018-01-08 15:17:03 -0500

    Setup validation spec for contact model

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do 
+    subject { described_class.new(customer: create(:customer), first_name: "Steve", last_name: "Jobs",
+                                  phone: "5555555555", fax: "5555555555", email: "steve@apple.com",
+                                  kind: 0, note: "Test notes") }
+
+    it "should be valid with valid attributes" do 
+      expect(subject).to be_valid
+    end
+
+    it "should not be valid without a customer" do 
+      subject.customer = nil 
+
+      expect(subject).to_not be_valid
+    end
+
+    it "should not be valid without a first name" do
+      subject.first_name = nil 
+
+      expect(subject).to_not be_valid
+    end
+
+    it "should not be valid without a last name" do
+      subject.last_name = nil 
+
+      expect(subject).to_not be_valid
+    end 
+
+    it "should not be valid without a phone number" do
+      subject.phone = nil 
+
+      expect(subject).to_not be_valid
+    end
+
+    it "should not be valid without an email address" do
+      subject.email = nil 
+
+      expect(subject).to_not be_valid
+    end
+  end 
 end
