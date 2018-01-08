ab8897d49a836cf565d171ad7508107cb98b7e54
speakingstardust <josh@autochemky.com>
2018-01-08 12:08:35 -0500

    Setup validations on address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,8 @@
 class Address < ActiveRecord::Base
+
+  validates :street, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true 
+  validates :category, presence: true
 end
