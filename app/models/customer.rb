60e289ebfbcb11e1a91953a3970525bfa1d1abab
speakingstardust <josh@autochemky.com>
2018-01-10 12:56:42 -0500

    Setup edit view and ensured that contacts is working.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,6 +5,7 @@ class Customer < ActiveRecord::Base
         has_many :scope_of_works
         has_many :document_mail_logs
         has_many :addresses, dependent: :destroy
+        has_many :contacts, dependent: :destroy
 
         accepts_nested_attributes_for :addresses, allow_destroy: true
 
